d7cc0ab6f3910dada02bf7a8f1752d7caa5dc70c
Rezna ghimire <laxmighimire202@gmail.com>
2022-05-07 19:02:31 +0545

    add comment

--- args_kwargs.py
+++ args_kwargs.py
@@ -1,5 +1,11 @@
 def fun(**kwargs):
+    """_summary_we created keyword arguments and it was passed in name 
+    and the reverse of the name was returned
+
+    Returns:
+        str: The name of the kwargs was returned in reverse
+    """
     return kwargs.get("name")[::-1]
 
-
+#print the function
 print(fun(name="Rezna"))
